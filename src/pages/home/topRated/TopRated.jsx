7c28abec46e48db372120d0fe61b32aa5bdf985d
Minh Tien <tiengiang951753@gmail.com>
2023-06-01 19:19:09 +0700

    Fix import carousel 2

--- src/pages/home/topRated/TopRated.jsx
+++ src/pages/home/topRated/TopRated.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import ContentWrapper from '../../../components/contentWrapper/ContentWrapper'
 import SwitchTabs from '../../../components/switchTabs/SwitchTabs';
-import Carousel from '../../../components/carousel/carousel';
+import Carousel from '../../../components/carousel/Carousel';
 import axios from 'axios';
 
 const TopRated = () => {
